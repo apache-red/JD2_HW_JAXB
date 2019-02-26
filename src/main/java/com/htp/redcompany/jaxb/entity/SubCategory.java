0d45176d7af50d83206b7264bff5906daf685a45
RED <apache_red@mail.ru>
2019-02-26 13:38:50 +0300

    add xml, xsd adnd entity

---
+++ src/main/java/com/htp/redcompany/jaxb/entity/SubCategory.java
@@ -0,0 +1,9 @@
+package com.htp.redcompany.jaxb.entity;
+
+import java.util.List;
+
+public class SubCategory {
+
+    private List<Movie> movieList;
+
+}
---
+++ src/main/java/com/htp/redcompany/jaxb/start/Start.java
@@ -0,0 +1,24 @@
+package com.htp.redcompany.jaxb.start;
+
+import com.htp.redcompany.jaxb.entity.Category;
+import com.htp.redcompany.jaxb.entity.SubCategory;
+
+public class Start {
+
+
+
+
+    public static void main(String[] args) {
+
+        Category category = new Category();
+        SubCategory subCategory =new SubCategory();
+
+
+
+    }
+
+
+
+
+
+}
